9775d3021bd7f6f244b47906a1af10cba98b1745
Gustavo Marialva <gmarialva@gmail.com>
2023-03-22 16:59:55 -0300

    conecta api, traduz json e setInterval

--- script/script.js
+++ script/script.js
@@ -13,3 +13,18 @@ const graficoParaDOlar = new Chart(graficoDolar, {
     ],
   },
 });
+setInterval(() => conectaAPI(), 5000);
+async function conectaAPI() {
+  const conecta = await fetch(
+    "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+  );
+  const conectaTraduzido = await conecta.json();
+  console.log(conectaTraduzido);
+}
+
+// Em resumo, construímos uma requisição do tipo GET para uma API. Como não sabemos se o retorno será rápido ou demorado, transformamos essa função em assíncrona. Desse modo, conseguimos esperar o retorno sem travar o restante do código.
+
+// Assim, temos uma conexão com uma biblioteca externa chamada Chart.js e uma requisição a uma API de cotação de moedas. Já conseguimos nos conectar com todos os recursos externos necessários para desenvolver nosso projeto.
+
+// **********************
+// Qual a função do setInterval()? Ele define um intervalo para algo acontecer, recebendo como parâmetro uma ação — neste caso, a chamada da API — junto ao tempo de duração de cada ciclo em milissegundos — neste caso, 5000 milissegundos que é equivalente à 5 segundos.
